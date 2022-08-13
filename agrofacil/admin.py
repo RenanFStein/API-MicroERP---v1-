5e8a2a383d7e01670bb601274d02de71ea8e8eff
RenanFStein <renan_stein@hotmail.com>
2022-08-13 00:10:22 -0300

    Deploy da API

--- agrofacil/admin.py
+++ agrofacil/admin.py
@@ -29,7 +29,7 @@ class VendasAdmin(admin.ModelAdmin):
 admin.site.register(Vendas, VendasAdmin)
 
 class ComprasAdmin(admin.ModelAdmin):
-    list_display= ('id','fornecedor','nome_fornecedor', 'produto','quantidade','valor', 'total_compra')
+    list_display= ('id','fornecedor', 'produto','quantidade','valor', 'total_compra')
     list_display_links= ('fornecedor',)
     list_filter = ('fornecedor',)     
 admin.site.register(Compras, ComprasAdmin) 
